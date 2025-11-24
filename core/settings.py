761398ff876fcb4994d0a7084f32a3088740d60f
Zamzamke <zamzamchelimo@gmail.com>
2025-11-24 16:06:21 +0300

    change settings for deployment

--- core/settings.py
+++ core/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'secret_key_value')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'uni-life.onrender.com,localhost,127.0.0.1').split(',')
 
 
 # Application definition
