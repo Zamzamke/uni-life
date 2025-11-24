a77acaad30606fb7bf8bdf8f658a1f2bbbba2e55
Zamzamke <zamzamchelimo@gmail.com>
2025-11-25 01:22:27 +0300

    update urls for documantation

--- core/urls.py
+++ core/urls.py
@@ -60,7 +60,7 @@ urlpatterns = [
     path('api/opportunities/', include('opportunities.urls')),
     
     # API Documentation
-    #path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
